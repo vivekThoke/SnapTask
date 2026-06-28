60b5a85b5dc025219e225ce97f523c97bb5b853e
vivekThoke <vivekthoke5@gmail.com>
2026-06-28 19:27:15 +0530

    added databse engine and connection session; base class for models (tables)

---
+++ backend/app/database/session.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
+
+engine = create_engine(
+    settings.database_url,
+    echo=True
+)
+
+SessionLocal = sessionmaker(
+    autoflush=False,
+    autocommit=False,
+    bind=engine
+)
+
