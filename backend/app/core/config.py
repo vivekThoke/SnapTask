523c9ee9fcabdc4988fd11372fbafcc3a571a29d
vivekThoke <vivekthoke5@gmail.com>
2026-06-28 18:44:54 +0530

    configured the database url

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,10 +6,27 @@ class Settings(BaseSettings):
     app_env: str
     debug: bool
     
+    database_host: str
+    database_port: str
+    database_name: str
+    database_user: str
+    database_password: str
+    
     model_config = SettingsConfigDict(
         env_file=".env",
         case_sensitive=False
     )
+    
+    @property
+    def database_url(self):
+        return (
+            f"postgresql://"
+            f"{self.database_user}:"
+            f"{self.database_password}@"
+            f"{self.database_host}:"
+            f"{self.database_port}/"
+            f"{self.database_name}"
+        )
     
     
 settings = Settings()
