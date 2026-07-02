b156222b806cc1129b9c98073f1b5011fc9d65d3
vivekThoke <vivekthoke5@gmail.com>
2026-07-02 22:21:01 +0530

    added user modle

---
+++ backend/app/models/user.py
@@ -0,0 +1,27 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    
+    username: Mapped[str] = mapped_column(
+        String(100),
+        unique=True,
+        nullable=False
+    )
+    
+    
+    email: Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        nullable=False
+    )
+    
+    hashed_password: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False
+    )
